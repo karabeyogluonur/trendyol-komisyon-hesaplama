51135d214d4c2c1f4c4a775930d05cbd8da146ec
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2025-12-21 00:42:31 +0300

    feat(Presentation): integrate FluentValidation

--- src/TKH.Presentation/Program.cs
+++ src/TKH.Presentation/Program.cs
@@ -1,6 +1,9 @@
 using TKH.DataAccess;
 using TKH.Business;
 using TKH.Core;
+using FluentValidation.AspNetCore;
+using FluentValidation;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 var mvcBuilder = builder.Services.AddControllersWithViews();
@@ -8,10 +11,27 @@ var mvcBuilder = builder.Services.AddControllersWithViews();
 if (builder.Environment.IsDevelopment())
     mvcBuilder.AddRazorRuntimeCompilation();
 
+
+#region Layer Service Registration
+
 builder.Services.AddCoreServices();
 builder.Services.AddDataAccessServices(builder.Configuration);
 builder.Services.AddBusinessServices();
 
+#endregion
+
+#region Fluent Validation
+
+builder.Services.AddFluentValidationAutoValidation(config =>
+{
+    config.DisableDataAnnotationsValidation = true;
+});
+
+builder.Services.AddFluentValidationClientsideAdapters();
+
+builder.Services.AddValidatorsFromAssemblyContaining<Program>();
+
+#endregion
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
