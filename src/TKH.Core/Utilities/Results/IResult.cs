1d2e875bd5ba1420854cf97b8836d2f0bd133bc8
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2025-12-20 22:11:42 +0300

    feat(Core): implement Result Pattern for standardized operation results

---
+++ src/TKH.Core/Utilities/Results/IResult.cs
@@ -0,0 +1,8 @@
+namespace TKH.Core.Utilities.Results
+{
+    public interface IResult
+    {
+        bool Success { get; }
+        string Message { get; }
+    }
+}
---
+++ src/TKH.Core/Utilities/Results/SuccessResult.cs
@@ -0,0 +1,8 @@
+namespace TKH.Core.Utilities.Results
+{
+    public class SuccessResult : Result
+    {
+        public SuccessResult(string message) : base(true, message) { }
+        public SuccessResult() : base(true) { }
+    }
+}
