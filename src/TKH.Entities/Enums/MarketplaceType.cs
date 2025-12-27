bdace967e045b6eb8d57885a8f0275f59ac8b52f
Onur Karabeyoğlu <123563374+karabeyogluonur@users.noreply.github.com>
2025-12-28 01:12:22 +0300

    refactor(integration): cleanup marketplace enums

--- src/TKH.Entities/Enums/MarketplaceType.cs
+++ src/TKH.Entities/Enums/MarketplaceType.cs
@@ -2,9 +2,6 @@ namespace TKH.Entities.Enums
 {
     public enum MarketplaceType
     {
-        Trendyol = 1,
-        Hepsiburada = 2,
-        N11 = 3,
-        Amazon = 4,
+        Trendyol = 1
     }
 }
